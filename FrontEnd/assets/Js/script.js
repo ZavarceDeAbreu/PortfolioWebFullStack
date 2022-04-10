cc844bddfac851f14b252c5462e6925422f77b8f
Natali <dayanna.zavarce@gmail.com>
2022-04-10 20:38:49 -0300

    terminacion del menu

--- FrontEnd/assets/Js/script.js
+++ FrontEnd/assets/Js/script.js
@@ -9,6 +9,15 @@
         $btnMenu.lastElementChild.classList.toggle("none");
         $menu.classList.toggle("is-active");
         $header.classList.toggle("is-active");
+    });
+
+    d.addEventListener("click" , e=>{
+        if(!e.target.matches(".menu a")) return false;
+        
+        $btnMenu.firstElementChild.classList.remove("none");
+        $btnMenu.lastElementChild.classList.add("none");
+        $menu.classList.remove("is-active");
+        $header.classList.remove("is-active");
     })
 })(document);
 
