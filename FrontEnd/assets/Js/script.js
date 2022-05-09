a037a2195e2a512b3158b5ae229b640bc529728d
natali <dayanna.zavarce@gmail.com>
2022-05-09 10:52:56 -0300

    creacion de la seccion About Me

--- FrontEnd/assets/Js/script.js
+++ FrontEnd/assets/Js/script.js
@@ -1,23 +1,51 @@
-/* -------- Menu --------- */
+
+//    $('.skills-content').waypoint(function() {
+//     $('.progress .progress-bar').each(function() {
+//         $(this).css("width", $(this).attr("aria-valuenow") + '%');
+//     });
+// }, {
+//     offset: '80%'
+// });
+
+/*--------AOS Animation--------*/
+AOS.init({
+  duration: 1000,
+  easing: "ease-in-out-back",
+  // once: true
+});
+/*------Sticky Navigation------*/
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("scroll", window.scrollY > 0);
+});
+/* ------------Menu----------- */
 ((d) => {
-    const $btnMenu = d.querySelector(".menu-btn"),
-    $header =d.querySelector(".header"),
-    $menu = d.querySelector(".menu");
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $navigation = d.querySelector(".navigation");
 
-    $btnMenu.addEventListener("click", (e) => {
-        $btnMenu.firstElementChild.classList.toggle("none");
-        $btnMenu.lastElementChild.classList.toggle("none");
-        $menu.classList.toggle("is-active");
-        $header.classList.toggle("is-active");
-    });
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $navigation.classList.toggle("is-active");
+  });
 
-    d.addEventListener("click" , e=>{
-        if(!e.target.matches(".menu a")) return false;
-        
-        $btnMenu.firstElementChild.classList.remove("none");
-        $btnMenu.lastElementChild.classList.add("none");
-        $menu.classList.remove("is-active");
-        $header.classList.remove("is-active");
-    })
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".navigation a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $navigation.classList.remove("is-active");
+  });
 })(document);
+/*-------Reveal Animation-------*/
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: "60px",
+});
+sr.reveal(".hero-text" , {display:600});
+sr.reveal(".hero-avatar" , {origin:"top" , display:700});
+/*---------Skill Bar-------------*/
 
+
+
+ 
