d5402a7e3285d3ec22a45a705b8eb8eb547e6a94
Natali <dayanna.zavarce@gmail.com>
2022-05-14 15:38:53 -0300

    adicion de boostrap

--- FrontEnd/Js/script.js
+++ FrontEnd/Js/script.js
@@ -1,105 +1,319 @@
 
-//    $('.skills-content').waypoint(function() {
-//     $('.progress .progress-bar').each(function() {
-//         $(this).css("width", $(this).attr("aria-valuenow") + '%');
-//     });
-// }, {
-//     offset: '80%'
-// });
-const prt_section = document.querySelector(".portfolio");
+/* const prt_section = document.querySelector(".portfolio");
 const zoom_icons = document.querySelectorAll(".zoom-icon");
 const modal_overlay = document.querySelector(".modal-overlay");
 const images = document.querySelectorAll(".images img");
 const prev_btn = document.querySelector(".prev-btn");
 const nex_btn = document.querySelector(".next-btn");
-
-
+ */
 /*--------AOS Animation--------*/
 AOS.init({
   duration: 1000,
   easing: "ease-in-out-back",
   once: true
 });
-/*------Sticky Navigation------*/
-window.addEventListener("scroll", function () {
-  let header = document.querySelector("header");
-  header.classList.toggle("scroll", window.scrollY > 0);
-});
-/* ------------Menu----------- */
-((d) => {
-  const $btnMenu = d.querySelector(".menu-btn"),
-    $navigation = d.querySelector(".navigation");
 
-  $btnMenu.addEventListener("click", (e) => {
-    $btnMenu.firstElementChild.classList.toggle("none");
-    $btnMenu.lastElementChild.classList.toggle("none");
-    $navigation.classList.toggle("is-active");
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+   const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
   });
 
-  d.addEventListener("click", (e) => {
-    if (!e.target.matches(".navigation a")) return false;
-
-    $btnMenu.firstElementChild.classList.remove("none");
-    $btnMenu.lastElementChild.classList.add("none");
-    $navigation.classList.remove("is-active");
+  /**
+   * Initiate glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
   });
-})(document);
-/*-------Reveal Animation-------*/
-let sr = ScrollReveal({
-  duration: 2500,
-  distance: "60px",
-});
-sr.reveal(".hero-text", { display: 600 });
-sr.reveal(".hero-avatar", { origin: "top", display: 700 });
-/*---------Skill Bar Animation-------------*/
 
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
 
-/*---------Portfolio Filter Animation-------------*/
-let mixer = mixitup(".portfolio-gallery", {
-  selectors: {
-    target: '.prt-card'
-  },
-  animation: {
-    duration: 500,
-  },
-});
+      let portfolioFilters = select('#portfolio-flters li', true);
 
-/*---------Modal Animation-------------*/
-let currentIndex = 0;
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
 
-zoom_icons.forEach((icn, i) => 
-icn.addEventListener("click", () => {
-  prt_section.classList.add("open");
-  document.body.classList.add("stopScrolling");
-  currentIndex = i;
-  changeImage(currentIndex);
-})
-);
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
 
-modal_overlay.addEventListener("click", () => {
-  prt_section.classList.remove("open");
-  document.body.classList.remove("stopScrolling");
-});
+      }, true);
+    }
 
-prev_btn.addEventListener("click", () => {
-  if(currentIndex === 0){
-    currentIndex = 5
-  }else{
-    currentIndex--;
-  }
-  changeImage(currentIndex);
-})
+  });
 
-nex_btn.addEventListener("click", () => {
-  if(currentIndex === 5){
-    currentIndex = 0
-  }else{
-    currentIndex++;
-  }
-  changeImage(currentIndex);
-})
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
 
-function changeImage(index) {
-images.forEach((img) => img.classList.remove("showImage"));
-images[index].classList.add("showImage");
-}
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+})()
+
+  /* ------------Menu----------- */
+  /* ((d) => {
+    const $btnMenu = d.querySelector(".menu-btn"),
+      $navigation = d.querySelector(".navigation");
+
+    $btnMenu.addEventListener("click", (e) => {
+      $btnMenu.firstElementChild.classList.toggle("none");
+      $btnMenu.lastElementChild.classList.toggle("none");
+      $navigation.classList.toggle("is-active");
+    });
+
+    d.addEventListener("click", (e) => {
+      if (!e.target.matches(".navigation a")) return false;
+
+      $btnMenu.firstElementChild.classList.remove("none");
+      $btnMenu.lastElementChild.classList.add("none");
+      $navigation.classList.remove("is-active");
+    });
+  })(document); */
+  /*-------Reveal Animation-------*/
+  /*---------Skill Bar Animation-------------*/
+
+  /*---------Modal Animation-------------*/
+ /*  let currentIndex = 0;
+
+  zoom_icons.forEach((icn, i) =>
+    icn.addEventListener("click", () => {
+      prt_section.classList.add("open");
+      document.body.classList.add("stopScrolling");
+      currentIndex = i;
+      changeImage(currentIndex);
+    })
+  );
+
+  modal_overlay.addEventListener("click", () => {
+    prt_section.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+  });
+
+  prev_btn.addEventListener("click", () => {
+    if (currentIndex === 0) {
+      currentIndex = 5
+    } else {
+      currentIndex--;
+    }
+    changeImage(currentIndex);
+  })
+
+  nex_btn.addEventListener("click", () => {
+    if (currentIndex === 5) {
+      currentIndex = 0
+    } else {
+      currentIndex++;
+    }
+    changeImage(currentIndex);
+  })
+
+  function changeImage(index) {
+    images.forEach((img) => img.classList.remove("showImage"));
+    images[index].classList.add("showImage");
+  } */
